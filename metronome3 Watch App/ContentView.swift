94d23f6608e4c49d8d64e02910c1d3d05d1117a0
tamaru2029 <tamaru2029@gmail.com>
2024-07-28 18:54:20 +0900

    Initial Commit

---
+++ metronome3 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  metronome3 Watch App
+//
+//  Created by hironori on 2024/07/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ metronome3 Watch App/metronome3App.swift
@@ -0,0 +1,17 @@
+//
+//  metronome3App.swift
+//  metronome3 Watch App
+//
+//  Created by hironori on 2024/07/28.
+//
+
+import SwiftUI
+
+@main
+struct metronome3_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
