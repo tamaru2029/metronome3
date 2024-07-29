dc50398e8e82e3957a172a0f3877d444f8446773
tamaru2029 <tamaru2029@gmail.com>
2024-07-29 10:40:23 +0900

    add button

--- metronome3 Watch App/ContentView.swift
+++ metronome3 Watch App/ContentView.swift
@@ -10,12 +10,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing:6){
+                /*@START_MENU_TOKEN@*/Text("Placeholder")/*@END_MENU_TOKEN@*/
+                Button{
+                    //ACTION
+                }label:{
+                    Image(systemName:"plus.circle")
+                        .font(.system(size: 42, weight: .semibold))
+                }
+                .fixedSize()
+                .buttonStyle(PlainButtonStyle())
+                .foregroundColor(.accentColor)
+            }
+            Spacer()
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-        }
-        .padding()
+        }//:VSTACK
+        .navigationTitle("TEST")
     }
 }
 
